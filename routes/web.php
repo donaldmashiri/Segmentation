7457e6e8ef125835f1846a3b687c75619606c9da
donaldmashiri <donaldtondemashiri@gmail.com>
2023-11-29 21:47:16 +0200

    o

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,16 @@ Auth::routes();
 Route::get('auth/google', [\App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle']);
 Route::get('login/google/callback', [\App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
 
-        Route::resource('customers', \App\Http\Controllers\CustomerController::class);
-        Route::resource('transactions', \App\Http\Controllers\TransactionController::class);
-        Route::resource('segment', \App\Http\Controllers\SegmentController::class);
-        Route::resource('results', \App\Http\Controllers\ResultAnalysisController::class);
-        Route::resource('report', \App\Http\Controllers\ReportController::class);
-        Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('customers', \App\Http\Controllers\CustomerController::class);
+    Route::resource('transactions', \App\Http\Controllers\TransactionController::class);
+    Route::resource('segment', \App\Http\Controllers\SegmentController::class);
+    Route::resource('results', \App\Http\Controllers\ResultAnalysisController::class);
+    Route::resource('report', \App\Http\Controllers\ReportController::class);
+    Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
+    Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
 
-        Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
